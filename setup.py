8130b32b1a9b1f397a6f7d09c86f2a305aabb1dc
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-07-13 15:38:37 +0100

    Add new classes to package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
     "pytest"]
 
 setup(name="orderlyweb-api",
-      version="0.0.4",
+      version="0.0.5",
       description="Python client for OrderlyWeb API",
       long_description=long_description,
       classifiers=[
