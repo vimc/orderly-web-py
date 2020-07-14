7432c3750d29d7906b75d40de86b835537953493
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-07-14 17:35:47 +0100

    user orderlyweb deploy

--- scripts/orderlyweb_cli.sh
+++ scripts/orderlyweb_cli.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 image=${REGISTRY}/orderly-web-user-cli:master
-docker run --rm -v montagu_orderly_volume:/orderly --network ${NETWORK} $image $@
+docker run --rm -v orderly_volume:/orderly --network ${NETWORK} $image $@
